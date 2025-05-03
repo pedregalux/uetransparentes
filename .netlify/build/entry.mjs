2bfb84b90d9d789c013a749122d1197c623896de
pedregalux <felipe.perry@vivaldi.net>
2025-05-03 17:37:42 -0400

    netlify

---
+++ .netlify/build/entry.mjs
@@ -0,0 +1,74 @@
+import { renderers } from './renderers.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
+import { manifest } from './manifest_B7j4IfBh.mjs';
+import { createExports } from '@astrojs/netlify/ssr-function.js';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/1.astro.mjs');
+const _page1 = () => import('./pages/10.astro.mjs');
+const _page2 = () => import('./pages/11.astro.mjs');
+const _page3 = () => import('./pages/12.astro.mjs');
+const _page4 = () => import('./pages/13.astro.mjs');
+const _page5 = () => import('./pages/14.astro.mjs');
+const _page6 = () => import('./pages/15.astro.mjs');
+const _page7 = () => import('./pages/2.astro.mjs');
+const _page8 = () => import('./pages/3.astro.mjs');
+const _page9 = () => import('./pages/4.astro.mjs');
+const _page10 = () => import('./pages/5.astro.mjs');
+const _page11 = () => import('./pages/6.astro.mjs');
+const _page12 = () => import('./pages/7.astro.mjs');
+const _page13 = () => import('./pages/8.astro.mjs');
+const _page14 = () => import('./pages/9.astro.mjs');
+const _page15 = () => import('./pages/justicia.astro.mjs');
+const _page16 = () => import('./pages/medioambiente.astro.mjs');
+const _page17 = () => import('./pages/participacion.astro.mjs');
+const _page18 = () => import('./pages/probidad.astro.mjs');
+const _page19 = () => import('./pages/proyecto.astro.mjs');
+const _page20 = () => import('./pages/seguimiento.astro.mjs');
+const _page21 = () => import('./pages/transparencia.astro.mjs');
+const _page22 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["src/pages/1.astro", _page0],
+    ["src/pages/10.astro", _page1],
+    ["src/pages/11.astro", _page2],
+    ["src/pages/12.astro", _page3],
+    ["src/pages/13.astro", _page4],
+    ["src/pages/14.astro", _page5],
+    ["src/pages/15.astro", _page6],
+    ["src/pages/2.astro", _page7],
+    ["src/pages/3.astro", _page8],
+    ["src/pages/4.astro", _page9],
+    ["src/pages/5.astro", _page10],
+    ["src/pages/6.astro", _page11],
+    ["src/pages/7.astro", _page12],
+    ["src/pages/8.astro", _page13],
+    ["src/pages/9.astro", _page14],
+    ["src/pages/justicia.astro", _page15],
+    ["src/pages/medioambiente.astro", _page16],
+    ["src/pages/participacion.astro", _page17],
+    ["src/pages/probidad.astro", _page18],
+    ["src/pages/proyecto.astro", _page19],
+    ["src/pages/seguimiento.astro", _page20],
+    ["src/pages/transparencia.astro", _page21],
+    ["src/pages/index.astro", _page22]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./_noop-actions.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "5d859b55-e30c-4a22-8ac0-283fe7fe6bbb"
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
+
+import netlify from '@astrojs/netlify';
+
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: netlify()
 });
