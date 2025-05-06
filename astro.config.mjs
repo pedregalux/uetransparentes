c9f9a488363487b25423867d19230fe831d4d211
pedregalux <felipe.perry@vivaldi.net>
2025-05-05 21:53:48 -0400

    save

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
+
+
+import netlify from '@astrojs/netlify';
 
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
+  adapter: netlify()
 });
